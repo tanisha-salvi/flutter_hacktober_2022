31f585fb49c275e5ea11b400a8dec64ade299102
Kunalpal215 <kunalpal215@gmail.com>
2022-10-04 14:17:08 +0530

    added starter code

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:campus_ola/screens/auth/signin.dart';
+import 'package:campus_ola/screens/auth/signup.dart';
+import 'package:campus_ola/screens/home_screen.dart';
+import 'package:campus_ola/stores/login_store.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(Provider(create: (_) => LoginStore(),child: const MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: SigninScreen.id,
+      routes: {
+        HomeScreen.id : (context) => const HomeScreen(),
+        SignupScreen.id : (context) => const SignupScreen(),
+        SigninScreen.id : (context) => const SigninScreen(),
+      },
+    );
+  }
+}
+
