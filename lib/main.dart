0bd31c2c8507413c65d6a7065aa1593caf397445
Ekaksh Janweja <jekaksh@gmail.com>
2022-10-11 18:55:52 +0530

    UI: ui for campus ola 5 page created and users redirected to this page from campus ola 1 page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:campus_ola/firebase_options.dart';
 import 'package:campus_ola/screens/auth/signin.dart';
 import 'package:campus_ola/screens/auth/signup.dart';
+import 'package:campus_ola/screens/campus_ola_5/campus_ola_five.dart';
 import 'package:campus_ola/screens/home_screen.dart';
 import 'package:campus_ola/stores/login_store.dart';
 import 'package:flutter/material.dart';
@@ -8,8 +10,8 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(Provider(create: (_) => LoginStore(),child: const MyApp()));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(Provider(create: (_) => LoginStore(), child: const MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -25,11 +27,11 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       initialRoute: SignInScreen.id,
       routes: {
-        HomeScreen.id : (context) => const HomeScreen(),
-        SignupScreen.id : (context) => const SignupScreen(),
-        SignInScreen.id : (context) => const SignInScreen(),
+        HomeScreen.id: (context) => const HomeScreen(),
+        SignupScreen.id: (context) => const SignupScreen(),
+        SignInScreen.id: (context) => const SignInScreen(),
+        CampusOlaFive.id: (context) => const CampusOlaFive(),
       },
     );
   }
 }
-
