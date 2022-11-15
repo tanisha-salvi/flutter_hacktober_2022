62d78808bc742b55c069bde8ee8ead53bf63efc5
Hareesh nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-11-15 16:57:47 +0530

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:campus_ola/screens/auth/signin.dart';
 import 'package:campus_ola/screens/auth/signup.dart';
-import 'package:campus_ola/screens/home_screen.dart';
+import 'package:campus_ola/screens/home.dart';
 import 'package:campus_ola/stores/login_store.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
