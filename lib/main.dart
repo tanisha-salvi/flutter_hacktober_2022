7bf99860aed92c9f950057bdfd66036e4907bf30
Hareesh nandigrama <85237630+Hareesh-Nandigrama@users.noreply.github.com>
2022-11-18 18:17:32 +0530

    Made all the widgets private and exported only necessary widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
-import 'package:campus_ola/screens/auth/signin.dart';
-import 'package:campus_ola/screens/auth/signup.dart';
-import 'package:campus_ola/screens/home.dart';
-import 'package:campus_ola/stores/login_store.dart';
+import 'package:cab_sharing/cab_sharing.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(Provider(create: (_) => LoginStore(),child: const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -22,13 +16,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: HomeScreen.id,
-      routes: {
-        HomeScreen.id : (context) => const HomeScreen(),
-        SignupScreen.id : (context) => const SignupScreen(),
-        SignInScreen.id : (context) => const SignInScreen(),
-      },
+    return const MaterialApp(
+      home: CabSharingScreen(userData: {'Name': 'name'}),
     );
   }
 }
